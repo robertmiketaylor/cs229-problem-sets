82c759d0d731dc7f0b008c2cf28c95f976243875
robertmiketaylor <robertmiketaylor@gmail.com>
2024-07-25 13:23:13 -0400

    initial commit

---
+++ problem-sets/PS1/src/p01b_logreg.py
@@ -0,0 +1,88 @@
+import numpy as np
+import util
+
+from linear_model import LinearModel
+
+
+def main(train_path, eval_path, pred_path):
+    """Problem 1(b): Logistic regression with Newton's Method.
+
+    Args:
+        train_path: Path to CSV file containing dataset for training.
+        eval_path: Path to CSV file containing dataset for evaluation.
+        pred_path: Path to save predictions.
+    """
+    x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+
+    # *** START CODE HERE ***
+    
+    # Train logistic regression
+    model = LogisticRegression(eps=1e-5)
+    model.fit(x_train, y_train)
+    
+    # Plot data and decision boundary
+    util.plot(x_train, y_train, model.theta, 'output/p01b_{}.png'.format(pred_path[-5]))
+    
+    # Save predictions
+    x_eval, y_eval = util.load_dataset(eval_path, add_intercept=True)
+    y_pred = model.predict(x_eval)
+    np.savetxt(pred_path, y_pred > 0.5, fmt='%d')
+    
+    # *** END CODE HERE ***
+
+
+class LogisticRegression(LinearModel):
+    """Logistic regression with Newton's Method as the solver.
+
+    Example usage:
+        > clf = LogisticRegression()
+        > clf.fit(x_train, y_train)
+        > clf.predict(x_eval)
+    """
+
+    def fit(self, x, y):
+        """Run Newton's Method to minimize J(theta) for logistic regression.
+
+        Args:
+            x: Training example inputs. Shape (m, n).
+            y: Training example labels. Shape (m,).
+        """
+        # *** START CODE HERE ***
+        
+        # Initialize theta
+        
+        m, n = x.shape
+        self.theta = np.zeros(n)
+        
+        # Newton's Method
+        
+        while True:
+            theta_old = np.copy(self.theta)
+            
+            # Compute Hessian Matrix
+            h_x = 1 / (1 + np.exp(-x.dot(self.theta))) # hypothesis
+            H = (x.T * h_x * (1 - h_x)).dot(x) / m     # Hessian
+            gradient_J_theta = x.T.dot(h_x - y) / m    # loss function
+            
+            self.theta -= np.linalg.inv(H).dot(gradient_J_theta) # update theta
+            
+            # End training
+            if np.linalg.norm(self.theta-theta_old, ord=1) < self.eps:
+                break
+        
+        # *** END CODE HERE ***
+
+    def predict(self, x):
+        """Make a prediction given new inputs x.
+
+        Args:
+            x: Inputs of shape (m, n).
+
+        Returns:
+            Outputs of shape (m,).
+        """
+        # *** START CODE HERE ***
+        
+        return 1 / (1 + np.exp(-x.dot(self.theta)))
+        
+        # *** END CODE HERE ***
